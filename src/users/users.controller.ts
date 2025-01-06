78ea248f3f1da21372d684c8577d15cc5d3aec1b
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-01-06 16:06:31 +0700

    Add users module
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findFall(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
